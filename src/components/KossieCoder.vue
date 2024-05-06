f41bfadcfa9e7cde67ed60f42393889fb951a6ef
SONGMIN <dhthdals@gmail.com>
2024-05-07 00:07:52 +0900

    18강 - 슬롯(Slot)

--- src/components/KossieCoder.vue
+++ src/components/KossieCoder.vue
@@ -1,8 +1,13 @@
 <template>
     <div>
-        <h1>{{ title }}</h1>
-        <p>{{ name }}</p><br>
-        <!-- <button @click="updateName">Change Name</button> -->
+        <p>header</p>
+        <slot name="header" :kossie="kossie"></slot> <!-- slot에 name을 지정하면 그 자리에 template로 대체된다. -->
+        <!-- kossie라는 이름으로 KossieCoder의 data kossie를 올려보내면 
+            AboutView에서 kossie라는 이름으로 사용할 수 있다. -->
+        <p>body</p>
+        <slot></slot> <!-- slot에 name을 지정하지 않으면 default -->
+        <p>footer</p>
+        <!-- Slot: 코드 밑에 원하는 html 코드를 따로 넣고 싶을 때 사용-->
     </div>
 </template>
 
@@ -21,9 +26,10 @@ export default ({
     },
 
     data() {
-        // return {
+        return {
+            kossie: 'coder',
         //     //name: 'Kossie Coder',
-        // }
+        }
     },
 
     methods: {
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <KossieCoder>
+        <!-- <template v-slot:header> --> <!-- KossieCoder의 header라는 이름은 가진 slot으로 대체된다. -->
+        <!-- # 으로 대체 가능하다. -->
+          <template #header="{ kossie }"> 
+          <p>header11 {{ kossie }}</p>
+          <!-- KossieCoder.vue에서 보낸 kossie라는 속성을 받아서 사용할 수 있다. -->
+        </template>
+
+        <template v-slot:default>
+          hello11
+        </template>
+    </KossieCoder>
   </div>
 </template>
 
 <script> 
+import KossieCoder from '@/components/KossieCoder.vue';
+
 export default {
+  components: {
+    KossieCoder
+  },
   
 }
 </script>
